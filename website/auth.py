e22d9bd00a62f4f33e04113c9cc2808b1e077d07
João Pedras <joaoliveirapedras@gmail.com>
2021-03-12 19:08:08 +0000

    sign-up html

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login_page():
 
-    return 'Login Page'
+    return render_template('login.html')
 
 @auth.route('sign-up')
 def signup_page():
 
-    return 'Sign-up Page'
+    return render_template("sign_up.html")
 
 @auth.route('logout')
 def logout_page():
