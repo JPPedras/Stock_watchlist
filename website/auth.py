377367a6f62785ff6e430e5fd78ab0f81a31b7de
João Pedras <joaoliveirapedras@gmail.com>
2021-03-12 20:01:16 +0000

    flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login_page():
+
 
     return render_template('login.html')
 
-@auth.route('sign-up')
+@auth.route('sign-up',methods=['GET', 'POST'])
 def signup_page():
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email too short.', category='error')
+        elif password1 != password2:
+            flash('Passwords dont match.', category='error')
+        elif len(password1) < 6:
+            flash('Password too short.', category='error')
+        elif len(username) < 3:
+            flash('Username too short.', category='error')
+        else:
+            flash('Account created succesfully!.', category='success')
+        
 
     return render_template("sign_up.html")
 
